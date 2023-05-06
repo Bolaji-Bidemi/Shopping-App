6241145bc59b84034f7e1d445482618086130562
Bolaji Bidemi <bidemi.bolaji@decagon.dev>
2023-05-06 10:29:20 +0100

    Added files

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -15,10 +15,10 @@ const Auth = () => {
     <div className="container">
       <h1>Login</h1>{" "}
       <form onClick={handleSubmit}>
-        <label htmlFor="id">Id</label>
+        {/* <label htmlFor="id">Id</label>
         <input type="text" name="id" id="id" />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input type="password" name="password" id="password" /> */}
         <button className="login-btn" type="submit">
           Login
         </button>
